4305dc1d795fee61b5f43e88b64077c0a4a2c5a7
Nico Burniske <nicoburniske@gmail.com>
2025-03-17 07:53:43 -0400

    add title to epub

--- src/epub_builder.rs
+++ src/epub_builder.rs
@@ -62,7 +62,7 @@ pub fn build_epub(comic: &Comic) -> Result<()> {
     let html_files = create_html_files(&html_dir, &image_paths)?;
 
     // Create toc.ncx
-    create_toc_ncx(&oebps_dir, &cover_path, &html_files)?;
+    create_toc_ncx(&comic, &oebps_dir, &cover_path, &html_files)?;
 
     // Create content.opf
     create_content_opf(&oebps_dir, &cover_path, &html_files, &image_paths)?;
@@ -177,7 +177,12 @@ fn create_html_files(oebps_dir: &Path, image_paths: &[PathBuf]) -> Result<Vec<Pa
 }
 
 /// Creates the toc.ncx file (navigation)
-fn create_toc_ncx(oebps_dir: &Path, cover_path: &Path, html_files: &[PathBuf]) -> Result<()> {
+fn create_toc_ncx(
+    c: &Comic,
+    oebps_dir: &Path,
+    cover_path: &Path,
+    html_files: &[PathBuf],
+) -> Result<()> {
     let toc_path = oebps_dir.join("toc.ncx");
     let uuid = Uuid::new_v4().to_string();
 
@@ -214,16 +219,16 @@ fn create_toc_ncx(oebps_dir: &Path, cover_path: &Path, html_files: &[PathBuf]) -
         r#"<?xml version="1.0" encoding="UTF-8"?>
 <ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
   <head>
-    <meta name="dtb:uid" content="{}"/>
+    <meta name="dtb:uid" content="{uuid}"/>
     <meta name="dtb:depth" content="1"/>
     <meta name="dtb:totalPageCount" content="0"/>
     <meta name="dtb:maxPageNumber" content="0"/>
   </head>
-  <docTitle><text>Comic Book</text></docTitle>
+  <docTitle><text>{title}</text></docTitle>
   <navMap>
-{}  </navMap>
+{nav_points}  </navMap>
 </ncx>"#,
-        uuid, nav_points
+        title = &c.title
     );
 
     let mut file = File::create(&toc_path)?;
